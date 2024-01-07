7e605596331705aa6fcb10fb33f504f38a7f1364
Marie Vouriot <mvouriot@outlook.com>
2024-01-07 21:48:23 +0100

    move distance class in other file

--- istream/src/main/scala/DistanceStream.scala
+++ istream/src/main/scala/DistanceStream.scala
@@ -8,32 +8,6 @@ import zio.json.ast.JsonCursor
 import javax.print.attribute.standard.Destination
 
 object HttpStream extends ZIOAppDefault {
-  case class Distance(text: String, value: Int)
-  case class Duration(text: String, value: Int)
-  case class Element(distance: Distance, duration: Duration, status: String)
-  case class Row(elements: List[Element])
-  case class DistanceResponse(destination_addresses: List[String], origin_addresses: List[String], rows: List[Row], status: String)
-
-  object Distance {
-    implicit val distanceDecoder: JsonDecoder[Distance] = DeriveJsonDecoder.gen[Distance]
-  }
-
-  object Duration {
-    implicit val durationDecoder: JsonDecoder[Duration] = DeriveJsonDecoder.gen[Duration]
-  }
-
-  object Element {
-    implicit val elementDecoder: JsonDecoder[Element] = DeriveJsonDecoder.gen[Element]
-  }
-
-  object Row {
-    implicit val rowDecoder: JsonDecoder[Row] = DeriveJsonDecoder.gen[Row]
-  }
-
-  object DistanceResponse {
-    implicit val distanceResponseDecoder: JsonDecoder[DistanceResponse] = DeriveJsonDecoder.gen[DistanceResponse]
-  }
-
   def fetchData() = {
     val url = URL
       .decode(
