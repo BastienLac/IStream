7838570367d638225f1f4bd45a08f8c128c05bd0
Tommy0246 <tomchabas1@gmail.com>
2024-01-07 19:55:16 +0100

    parse json from api

--- istream/src/main/scala/DistanceStream.scala
+++ istream/src/main/scala/DistanceStream.scala
@@ -2,8 +2,38 @@ import zio._
 import zio.http._
 import zio.stream._
 import zio.Duration._
+import zio.json._
+import scala.compiletime.ops.string
+import zio.json.ast.JsonCursor
+import javax.print.attribute.standard.Destination
 
 object HttpStream extends ZIOAppDefault {
+  case class Distance(text: String, value: Int)
+  case class Duration(text: String, value: Int)
+  case class Element(distance: Distance, duration: Duration, status: String)
+  case class Row(elements: List[Element])
+  case class DistanceResponse(destination_addresses: List[String], origin_addresses: List[String], rows: List[Row], status: String)
+
+  object Distance {
+    implicit val distanceDecoder: JsonDecoder[Distance] = DeriveJsonDecoder.gen[Distance]
+  }
+
+  object Duration {
+    implicit val durationDecoder: JsonDecoder[Duration] = DeriveJsonDecoder.gen[Duration]
+  }
+
+  object Element {
+    implicit val elementDecoder: JsonDecoder[Element] = DeriveJsonDecoder.gen[Element]
+  }
+
+  object Row {
+    implicit val rowDecoder: JsonDecoder[Row] = DeriveJsonDecoder.gen[Row]
+  }
+
+  object DistanceResponse {
+    implicit val distanceResponseDecoder: JsonDecoder[DistanceResponse] = DeriveJsonDecoder.gen[DistanceResponse]
+  }
+
   def fetchData() = {
     val url = URL
       .decode(
@@ -25,12 +55,12 @@ object HttpStream extends ZIOAppDefault {
           for {
             res <- z
             body <- res.body.asString
-            _ <- Console.printLine(s"body size is: ${body.length}")
-          } yield body
+            resultOf = body.fromJson[DistanceResponse]
+            _ <- Console.printLine(s"body size is: ${body.length()}")
+          } yield resultOf
         }
         .foreach(Console.printLine(_))
     } yield ()
 
     appLogic.provide(Client.default, Scope.default)
-
 }
