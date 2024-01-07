158f38e0f44d618639c7644df6434c47ee24bac0
Bastien <bastien@lacour75.com>
2024-01-07 15:20:19 +0100

    fix Api url

--- istream/src/main/scala/DistanceStream.scala
+++ istream/src/main/scala/DistanceStream.scala
@@ -7,21 +7,20 @@ object HttpStream extends ZIOAppDefault {
   def fetchData() = {
     val url = URL
       .decode(
-        // "https://distance-calculator.p.rapidapi.com/distance/simple?lat_1=47.373535&long_1=8.541109&lat_2=42.335321&long_2=-71.023516&unit=miles&decimal_places=2"
+        "https://maps.googleapis.com/maps/api/distancematrix/json?origins=Paris&destinations=Lyon&key=AIzaSyB9UK97M3nPsMismYLKUSArWMFa80Ry_cw"
       )
       .toOption
       .get
 
     for {
       client <- ZIO.service[Client]
-      res <- client.url(url).get("/")
+      res <- client.url(url).get("")
     } yield res
   }
 
   override def run: ZIO[Any, Any, Unit] =
     val appLogic = for {
       _ <- ZStream(fetchData())
-        .repeat(Schedule.spaced(10.seconds))
         .mapZIO { z =>
           for {
             res <- z
